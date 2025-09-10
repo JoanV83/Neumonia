0c67cbb8349e8ac18fd99df325720d1a7a9d654a
Edwin Vicente Zapata <edwin_vic.zapata@uao.edu.co>
2025-09-09 23:48:02 -0500

    Se corrige archivo test_model

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,8 +3,7 @@ import pytest
 import tensorflow as tf
 from src.load_model import load_model
 
-# Ajusta el nombre del archivo a tu modelo real
-MODEL_PATH = "models/WilhemNet86.h5"
+MODEL_PATH = "models/conv_MLP_84.h5"
 
 @pytest.mark.skipif(not os.path.exists(MODEL_PATH), reason="modelo .h5 no disponible")
 def test_model_loading():
